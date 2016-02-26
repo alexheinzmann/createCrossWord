66ab1d8376fbdd3fba6d4c3ee7f3e7a0ed9f3ec0
Alexander Heinzmann <alex.heinzmann@gmail.com>
2016-02-25 22:20:29 -0600

    added psuedo code to importVocab.py

--- importVocab.py
+++ importVocab.py
@@ -2,11 +2,28 @@
 #identify single word highlights, and the create a map pairing words with
 #definitions
 
-#
-inputFilePath = '';
+#temporarily just read in My Clippings.txt from this directory
+inputFilePath = 'My Clippings.txt'
 
-#
-inputFile = open(inputFilePath, 'r');
+#open the file
+inputFile = open(inputFilePath, 'r')
 
-#iterate through file and find single-word highlights
+#test
+print inputFile.readline()
+print inputFile.readline()
+
+#iterate through file and find single-word highlights. 
+
+	#find next line blank line. The following line is the quote
+
+	#store the quote in variable "quote"
+
+	#strip quote of extra characters (.,!- etc.)
+
+	#if quote is single word
+		#lookup dictionary definition in kindle
+		#add to quote_dictionary
+
+#iterate through file and add new quotes to user's database
+
 
