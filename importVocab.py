7b602263029c0dec5fa0712c57ad51bb4a55efa0
Alexander Heinzmann <alex.heinzmann@gmail.com>
2016-02-25 21:59:22 -0600

    Adding new files My Clippings.txt and importVocab.py

---
+++ importVocab.py
@@ -0,0 +1,12 @@
+#This class will check to see if a Kindle is plugged in, pull the highlights,
+#identify single word highlights, and the create a map pairing words with
+#definitions
+
+#
+inputFilePath = '';
+
+#
+inputFile = open(inputFilePath, 'r');
+
+#iterate through file and find single-word highlights
+
